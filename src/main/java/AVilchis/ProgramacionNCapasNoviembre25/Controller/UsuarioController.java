7319144e608cdc0de90d79c38fe46d120e98f1fe
mariovilchis <mariovilchis01@hotmail.com>
2025-12-26 16:56:16 -0600

    Cliente/FormAdd

--- src/main/java/AVilchis/ProgramacionNCapasNoviembre25/Controller/UsuarioController.java
+++ src/main/java/AVilchis/ProgramacionNCapasNoviembre25/Controller/UsuarioController.java
@@ -9,21 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("usuario")
