f0c50329bc14af71028b21974a9982622d6570a1
mariovilchis <mariovilchis01@hotmail.com>
2025-12-29 18:37:08 -0600

    Cliente/FormUpdate

--- src/main/java/AVilchis/ProgramacionNCapasNoviembre25/Controller/UsuarioController.java
+++ src/main/java/AVilchis/ProgramacionNCapasNoviembre25/Controller/UsuarioController.java
@@ -1,6 +1,9 @@
 package AVilchis.ProgramacionNCapasNoviembre25.Controller;
 
+import AVilchis.ProgramacionNCapasNoviembre25.ML.Colonia;
 import AVilchis.ProgramacionNCapasNoviembre25.ML.Direccion;
+import AVilchis.ProgramacionNCapasNoviembre25.ML.Estado;
+import AVilchis.ProgramacionNCapasNoviembre25.ML.Municipio;
 import AVilchis.ProgramacionNCapasNoviembre25.ML.Pais;
 import AVilchis.ProgramacionNCapasNoviembre25.ML.Result;
 import AVilchis.ProgramacionNCapasNoviembre25.ML.Rol;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -71,25 +75,92 @@ public class UsuarioController {
         model.addAttribute("usuario", usuario);
         return "UsuarioForm";
     }
+    
+//     @PostMapping("/add")
+//    public String addUsuario(@Valid @ModelAttribute("Usuario") Usuario usuario,BindingResult bindingResult,
+//            @RequestParam("imagenFile") MultipartFile imagenFile, Model model) {
+//
+//        if (bindingResult.hasErrors()) {
+//            model.addAttribute("Usuario", usuario);
+//            return "/FormUsu"; // Vuelve al formulario si hay errores
+//        }
+//
+//        
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+//        
+//        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+//
+//        // Parte 1: El objeto Usuario 
+//        HttpHeaders jsonHeaders = new HttpHeaders();
+//        jsonHeaders.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<Usuario> usuarioPart = new HttpEntity<>(usuario, jsonHeaders);
+//        body.add("usuario", usuarioPart);
+//
+//        // Parte 2: El archivo (si existe)
+//        if (imagenFile != null && !imagenFile.isEmpty()) {
+//            body.add("Imagen", imagenFile.getResource()); 
+//        }
+//
+//        // 3. Crear la petición completa
+//        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+//
+//        try {
+//            ResponseEntity<Result> response = restTemplate.exchange(
+//                    urlBase + "usuario/add",
+//                    HttpMethod.POST,
+//                    requestEntity,
+//                    Result.class
+//            );
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "redirect:/usuario";
+//    }
 
-    @PostMapping("/add")
-    public String AddUser(@ModelAttribute("usuario") Usuario usuario) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
+    @GetMapping("detail/{IdUsuario}")
+    public String Detail(@PathVariable("IdUsuario") int IdUsuario, Model model) {
+        
+        RestTemplate restTemplate = new RestTemplate();
 
-            // Llamada POST al API para agregar usuario
-            HttpEntity<Usuario> request = new HttpEntity<>(usuario);
-            ResponseEntity<Result> response = restTemplate.exchange(
-                    urlBase + "/agregar", // Endpoint de tu API que maneja POST
-                    HttpMethod.POST,
-                    request,
-                    Result.class
-            );
+        ResponseEntity<Result<Usuario>> responseEntity
+                = restTemplate.exchange(urlBase + "/usuario/" + IdUsuario, HttpMethod.GET,
+                        HttpEntity.EMPTY, new ParameterizedTypeReference<Result<Usuario>>() {
+                });
 
-        } catch (Exception e) {
+         ResponseEntity<Result<List<Rol>>> responseEntityRol
+                = restTemplate.exchange(urlBase + "/rol", HttpMethod.GET,
+                        HttpEntity.EMPTY, new ParameterizedTypeReference<Result<List<Rol>>>() {
+                });
 
+        Result resultRol = responseEntityRol.getBody();
+        model.addAttribute("Roles", resultRol.Object);
+
+        ResponseEntity<Result<List<Pais>>> responseEntityPais
+                = restTemplate.exchange(urlBase + "/pais", HttpMethod.GET,
+                        HttpEntity.EMPTY, new ParameterizedTypeReference<Result<List<Pais>>>() {
+                });
+
+        Result resultPais = responseEntityPais.getBody();
+        
+        if (responseEntity.getStatusCode().value() == 200) {
+            Result result = responseEntity.getBody();
+
+            model.addAttribute("Roles", resultRol.Object);
+
+            model.addAttribute("Paises", resultPais.Object);
+
+            model.addAttribute("usuario", result.Object);
+            
+            model.addAttribute("Direccion", new Direccion());
         }
-        return "redirect:/usuario";
+
+        return "UsuarioEditar";
     }
+    
 
 }
